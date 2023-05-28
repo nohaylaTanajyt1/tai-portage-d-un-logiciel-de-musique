00cbf7d5ff7668e5a164edfeb7c0d0faa89ecfcb
nohaylaTanajyt1 <nohaylatanajyt1@gmail.com>
2023-05-28 17:44:09 +0200

    Updated numpy version

--- app.py
+++ app.py
@@ -240,5 +240,9 @@ def adminModifOeuvre():
     else:
         return render_template('connexion.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    # Use the provided PORT environment variable, or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='.0.0.0', port=port)
