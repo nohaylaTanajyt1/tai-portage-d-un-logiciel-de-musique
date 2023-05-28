5657fc148c71d777596309aae0f308b721c17528
nohaylaTanajyt1 <nohaylatanajyt1@gmail.com>
2023-05-28 21:44:44 +0200

    updated

--- app.py
+++ app.py
@@ -245,4 +245,4 @@ def adminModifOeuvre():
 if __name__ == '__main__':
     # Use the provided PORT environment variable, or default to 5000
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
